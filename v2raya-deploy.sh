62b54aa198e8fb3584b6f2aea16d0de4af45bd51
Gxliu <74167178+kpol-lgx@users.noreply.github.com>
2024-04-18 15:51:26 +0800

    Update v2raya-deploy.sh
    add sudo for cp

--- v2raya-deploy.sh
+++ v2raya-deploy.sh
@@ -3,7 +3,7 @@
 cd resources
 
     unzip v2ray-linux-$(uname -m).zip -d ./v2ray
-    sudo mkdir -p /usr/local/share/v2ray && cp ./v2ray/*dat /usr/local/share/v2ray
+    sudo mkdir -p /usr/local/share/v2ray && sudo cp ./v2ray/*dat /usr/local/share/v2ray
     sudo install -Dm755 ./v2ray/v2ray /usr/local/bin/v2ray
     rm -rf ./v2ray
 
